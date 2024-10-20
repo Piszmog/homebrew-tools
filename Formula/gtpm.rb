8a8ea03dc389d5b83324f9834a8fd003a4e4849c
Randell <piszmogcode@gmail.com>
2024-10-19 21:36:09 -0600

    Try making go optional

--- Formula/gtpm.rb
+++ Formula/gtpm.rb
@@ -4,8 +4,9 @@ class Gtpm < Formula
   url "https://github.com/Piszmog/gtpm/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "dcc161417b4e11e6c058072dac87a1c3c96e6de23b68caa580cf16e300a19233"
   license "MIT"
+  head "https://github.com/Piszmog/gtpm.git", branch: "main"
 
-  depends_on "go" => :build
+  depends_on "go" => :build unless which("go")
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
