eb1618137b4e185cb3b39e970d53c2428a0acfde
Randell <piszmogcode@gmail.com>
2024-11-29 11:05:38 -0700

    Fix wiggle

--- Formula/wiggle.rb
+++ Formula/wiggle.rb
@@ -5,13 +5,18 @@ class Wiggle < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Piszmog/wiggle/releases/download/v1.0.0/wiggle_Darwin_x86_64.tar.gz"
-    sha256 "365aa3c47d9c4412015ba4ca203858e2c1d870b71e60aba92420650b113e9593"
+    url "https://github.com/Piszmog/wiggle/releases/download/v1.0.2/wiggle_Darwin_x86_64.tar.gz"
+    sha256 "7f6a759ea485ba0e88286c10ac15670f3f9d36e559e6f64715c08a0c143496b1"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Piszmog/wiggle/releases/download/v1.0.0/wiggle_Darwin_arm64.tar.gz"
-    sha256 "3408dd0a751bdc6585a4e81a7b465c6b53002616acb9020af601371fb21114b5"
+    url "https://github.com/Piszmog/wiggle/releases/download/v1.0.2/wiggle_Darwin_arm64.tar.gz"
+    sha256 "6ccd7cd26e05c2ae87d13d7d85345b42d82b393b9be82ca882059955c20f9841"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/Piszmog/wiggle/releases/download/v1.0.2/wiggle_Linux_x86_64.tar.gz"
+    sha256 "636653d16b0ec133be4ff77f990052fd509ef54947ea7a2fac856bb2eb3ab76c"
   end
 
   def install
